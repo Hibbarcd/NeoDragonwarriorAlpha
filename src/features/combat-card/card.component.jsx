b3c8b0fb9f67dc9298780a8640b21d595044e2ef
Black Jack <hibbarcd@gmail.com>
2020-06-14 09:44:47 -0400

    removed previous active classname logic

--- src/features/combat-card/card.component.jsx
+++ src/features/combat-card/card.component.jsx
@@ -79,20 +79,4 @@ export const Card = props => (
         <img className="enemy_img" src={`${monster}`} alt="monster"></img>
     </div>
 )
-const cards = document.getElementsByClassName("card-container")
- // Get all buttons with class="btn" inside the container
 
- // Loop through the buttons and add the active class to the current/clicked button
- for (var i = 0; i < cards.length; i++) {
-   cards[i].addEventListener("click", function() {
-     var current = document.getElementsByClassName("active");
- 
-     // If there's no active class
-     if (current.length > 0) {
-       current[0].className = current[0].className.replace(" active", "");
-     }
- 
-     // Add the active class to the current/clicked button
-     this.className += " active";
-   });
- }
